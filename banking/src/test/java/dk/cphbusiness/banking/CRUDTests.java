5de60d72d90a03ddb93004182493118f1fa45f18
mjaay1337 <mbj3400@gmail.com>
2020-04-26 16:14:55 +0200

    crudtests

--- banking/src/test/java/dk/cphbusiness/banking/CRUDTests.java
+++ banking/src/test/java/dk/cphbusiness/banking/CRUDTests.java
@@ -12,8 +12,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
