6da76ae9b9e5ed83d8aa12a83d9508d206bebd02
mjaay1337 <mbj3400@gmail.com>
2020-04-29 10:04:00 +0200

    converted to mysql script

--- banking/scripts/initbanking.sql
+++ banking/scripts/initbanking.sql
@@ -1,46 +1,44 @@
-DROP TABLE IF EXISTS banktest.bank CASCADE;
-DROP TABLE IF EXISTS banktest.account CASCADE;
 DROP TABLE IF EXISTS banktest.movement CASCADE;
 DROP TABLE IF EXISTS banktest.customer CASCADE;
-DROP SCHEMA IF EXISTS banktest CASCADE;
-
-SET search_path to banktest;
+DROP TABLE IF EXISTS banktest.account CASCADE;
+DROP TABLE IF EXISTS banktest.bank CASCADE;
+DROP SCHEMA IF EXISTS BankTest;
 
 CREATE SCHEMA BankTest;
-SET search_path to "BankTest";
+
 
 CREATE TABLE BankTest.Bank(
  	id SERIAL PRIMARY KEY,
-	cvr varchar, 
- 	name varchar);
+	cvr varchar(255), 
+ 	name varchar(255));
 	
 CREATE TABLE BankTest.Customer(
 	id SERIAL PRIMARY KEY,
-	cpr varchar,
-	name varchar,
-	bankid int references BankTest.Bank(id)ON DELETE CASCADE);
+	cpr varchar(255),
+	name varchar(255),
+	bankid int references Bank(id)ON DELETE CASCADE);
 	
 CREATE TABLE BankTest.Account(
 	id SERIAL PRIMARY KEY, 
-	bankid int references BankTest.Bank(id) ON DELETE CASCADE, 
-	customerid int references BankTest.Customer(id) ON DELETE CASCADE, 
-	accountNumber varchar, 
+	bankid int references Bank(id) ON DELETE CASCADE, 
+	customerid int references Customer(id) ON DELETE CASCADE, 
+	accountNumber varchar(255), 
 	balance int);
 
 CREATE TABLE BankTest.Movement(
 	id SERIAL PRIMARY KEY,
 	timeOfTransfer date,
 	amount int,
- 	targetAccount int references BankTest.Account(id) ON DELETE CASCADE,
-	sourceAccount int references BankTest.Account(id) ON DELETE CASCADE);
+ 	sourceAccount int references Account(id) ON DELETE CASCADE,
+	targetAccount int references Account(id) ON DELETE CASCADE);
 
 INSERT INTO BankTest.Bank (cvr,name) VALUES ('1234','Jyske Bank');
 INSERT INTO BankTest.Bank (cvr,name) VALUES ('4321','Danske Bank');
 
-INSERT INTO BankTest.Customer (cpr,name,bankid) VALUES ('1010102010','Mogens',1);
-INSERT INTO BankTest.Customer (cpr,name,bankid) VALUES ('2020201010','Jesper',2);
+INSERT INTO BankTest.Customer (cpr,name,id) VALUES ('1010102010','Mogens',1);
+INSERT INTO BankTest.Customer (cpr,name,id) VALUES ('2020201010','Jesper',2);
 
 INSERT INTO BankTest.Account (bankid,customerid,accountnumber,balance) VALUES (1,1,'ABC123',100);
 INSERT INTO BankTest.Account (bankid,customerid,accountnumber,balance) VALUES (2,2,'123ABC',200);
 
-INSERT INTO BankTest.Movement(timeoftransfer,amount,targetaccount,sourceaccount) VALUES (now(),100,1,2);
+INSERT INTO BankTest.Movement(timeoftransfer,amount,sourceAccount, targetAccount) VALUES (now(),100,1,2);
